537ea1bdfe3cbcbd30545a38dec5ec32d1d69256
maheedhar132 <45238528+maheedhar132@users.noreply.github.com>
2019-11-27 19:29:03 +0530

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,5 +1,5 @@
 def call(body){
-sh '(curl --request POST \ --url {'https://jira132.atlassian.net/rest/api/3/issue'}  --user '{mahitalluri132@gmail.com:kL4PmIMRZiwyjQchOfg33905}'  --header '{Accept: application/json}'  --header '{Content-Type: application/json}' --data '{'("fields": {
+sh '(curl --request POST  --url {'https://jira132.atlassian.net/rest/api/3/issue'}  --user '{mahitalluri132@gmail.com:kL4PmIMRZiwyjQchOfg33905}'  --header '{Accept: application/json}'  --header '{Content-Type: application/json}' --data '{'("fields": {
         "summary": "Summit 2019 is awesome!",
         "issuetype": {
             "id": "10303"
