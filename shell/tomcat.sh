a6cf2f6ce551d36ffb149d4268ae8bc2b6615ace
MaheedharTalluri <1210815148@gitam.in>
2019-12-09 18:31:32 +0530

    tomcat edited

--- shell/tomcat.sh
+++ shell/tomcat.sh
@@ -2,7 +2,7 @@
 var=$(sudo find / -name "version.sh")
 if [[ -z "$var" ]];then
 sudo mkdir /home/ec2-user/apache
-sudo cd /home/ec2-user/apache
+
 #download tomcat
 sudo yum install java
 
@@ -19,24 +19,20 @@ tar -xvzf apache-tomcat-8.5.49.tar.gz
 #delete tar file
 rm -rf apache-tomcat-8.5.49.tar.gz
 
-#copy to apache dir
-sudo cp -avr $WORKSPACE/apache-tomcat-8.5.49 /home/ec2-user/apache
-
-sudo rm -rf $WORKSPACE/apache-tomcat-8.5.49/
 
 
 
 #changing the port for tomcat
-sudo sed -i "s/port=\"8080\"/port=\"8888\"/" /home/ec2-user/apache/apache-tomcat-8.5.49/conf/server.xml
+sudo sed -i "s/port=\"8080\"/port=\"8888\"/" /apache-tomcat-8.5.49/conf/server.xml
 
 
 #tomcat-users
-sudo sed -i 's\</tomcat-users>\<!-- -->\g' /home/ec2-user/apache/apache-tomcat-8.5.49/conf/tomcat-users.xml
-sudo echo '<role rolename="manager-gui" />' >> /home/ec2-user/apache/apache-tomcat-8.5.49/conf/tomcat-users.xml
-sudo echo '<user username="admin" password="admin" roles="manager-gui" />' >> /home/ec2-user/apache/apache-tomcat-8.5.49/conf/tomcat-users.xml
-sudo echo '<role rolename="manager-script" />' >> /home/ec2-user/apache/apache-tomcat-8.5.49/conf/tomcat-users.xml
-sudo echo '<user username="script" password="script" roles="manager-script" />' >> /home/ec2-user/apache/apache-tomcat-8.5.49/conf/tomcat-users.xml
-sudo echo '</tomcat-users>' >> /home/ec2-user/apache/apache-tomcat-8.5.49/conf/tomcat-users.xml
+ sed -i 's\</tomcat-users>\<!-- -->\g' /apache-tomcat-8.5.49/conf/tomcat-users.xml
+ echo '<role rolename="manager-gui" />' >> /apache-tomcat-8.5.49/conf/tomcat-users.xml
+ echo '<user username="admin" password="admin" roles="manager-gui" />' >> /apache-tomcat-8.5.49/conf/tomcat-users.xml
+ echo '<role rolename="manager-script" />' >> /apache-tomcat-8.5.49/conf/tomcat-users.xml
+ echo '<user username="script" password="script" roles="manager-script" />' >> /apache-tomcat-8.5.49/conf/tomcat-users.xml
+ echo '</tomcat-users>' >> /apache-tomcat-8.5.49/conf/tomcat-users.xml
 
 
 #setting valve in comments
@@ -44,6 +40,13 @@ sudo echo '</tomcat-users>' >> /home/ec2-user/apache/apache-tomcat-8.5.49/conf/t
 sudo sed -i 's/<Valve /<!-- <Valve /' /home/ec2-user/apache/apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
 
 sudo sed -i 's\:1" />\:1" /> -->\g' /home/ec2-user/apache/apache-tomcat-8.5.49/webapps/manager/META-INF/context.xml
+
+#copy to apache dir
+sudo cp -avr $WORKSPACE/apache-tomcat-8.5.49 /home/ec2-user/apache
+
+sudo rm -rf $WORKSPACE/apache-tomcat-8.5.49/
+
+
 
 #running tomcat
 sudo /home/ec2-user/apache/apache-tomcat-8.5.49/bin/./startup.sh
